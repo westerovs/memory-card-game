a40e334960ef2fcefab8b3a4468e4706eaf74f54
Александр <westerovs@mail.ru>
2022-09-16 16:13:17 +0300

    create config

--- src/js/index.js
+++ src/js/index.js
@@ -1,34 +1,58 @@
-class Game {
-  constructor() {
-    this.game  = null
-    this.init()
-  }
-  
-  init() {
-    this.game = new Phaser.Game(
-      1366,
-      1366,
-      Phaser.CANVAS,
-      null,
-      {
-        preload: this.preload,
-        create : this.create,
-        update : this.update,
-      })
-  }
-  
-  preload = () => {
-    this.game.load.image('block1', './src/img/block1.png')
-  }
-  
-  create = () => {
-    this.game.add.sprite(100, 100, 'block1')
-  }
-  
-  update = () => {
-  
-  }
+/*
+this.game.config.width - размер игры
+*/
+const state = new Phaser.State('Game')
+
+state.preload = function () {
+  console.log('preload')
+  this.load.image('bg', './src/img/background.png')
 }
 
-new Game()
+state.create = function () {
+  this.add.sprite(0, 0, 'bg')
+  console.log(this.game)
+  console.log('create')
+}
+
+const config = {
+  renderer: Phaser.AUTO,
+  width: 1280,
+  height: 720,
+  state,
+  
+  // state: {
+  //   init: function() {
+  //     this.game.debug.font = '14px monospace';
+  //     this.game.debug.lineHeight = 18;
+  //   },
+  //
+  //   preload: function() {
+  //     this.load.baseURL = 'https://cdn.jsdelivr.net/gh/samme/phaser-examples-assets@v1.0.0/';
+  //     this.load.crossOrigin = 'anonymous';
+  //     this.load.image('dude', 'sprites/phaser-dude.png');
+  //     this.load.image('starfield', 'skies/starfield.png');
+  //   },
+  //
+  //   create: function() {
+  //     this.add.tileSprite(0, 0, 800, 800, 'starfield');
+  //     this.add.sprite(400, 300, 'dude');
+  //   },
+  //
+  //   render: function() {
+  //     var debug = this.game.debug;
+  //
+  //     // debug.object(this.game.config, 10, 20, {
+  //     //   label: 'game.config',
+  //     //   color: 'auto',
+  //     // });
+  //
+  //     debug.phaser(20, 780);
+  //   }
+  // }
+}
+
+window.game = new Phaser.Game(config);
+
+
+
 
